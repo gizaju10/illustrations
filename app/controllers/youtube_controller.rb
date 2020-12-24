5f6befa01acaeaaf499607e8a5fcda2bba7bcbe4
gizaju10 <kaminomi3@gmail.com>
2020-12-24 10:14:44 +0900

    YouTube検索機能の不具合修正

--- app/controllers/youtube_controller.rb
+++ app/controllers/youtube_controller.rb
@@ -1,51 +1,51 @@
 class YoutubeController < ApplicationController
-    require 'google/apis/youtube_v3'
-  
-    def find_videos(keyword, after: 1.months.ago, before: Time.now)
+  require 'google/apis/youtube_v3'
+
+  def find_videos(keyword, after: 1.months.ago, before: Time.now)
+  begin
+   service = Google::Apis::YoutubeV3::YouTubeService.new
+   service.key = ENV['YOUTUBE_2']
+
+   next_page_token = nil
+   opt = {
+     q: keyword,
+     type: 'video',
+     max_results: 2,
+     order: :date,
+     page_token: next_page_token,
+     published_after: after.iso8601,
+     published_before: before.iso8601
+   }
+   service.list_searches(:snippet, opt)
+  rescue Google::Apis::ClientError
+    flash[:alert] = "１日の上限に達しました。"
+    redirect_to(root_path)
+  end
+  end
+
+  def index
+    @youtube_data = find_videos('焼まゆるのお絵かきちゃんねる')
+    @post = Post.new
+  end
+
+
+  def indexx
     begin
-     service = Google::Apis::YoutubeV3::YouTubeService.new
-     service.key = ENV['YOUTUBE_2']
-  
-     next_page_token = nil
-     opt = {
-       q: keyword,
-       type: 'video',
-       max_results: 2,
-       order: :date,
-       page_token: next_page_token,
-       published_after: after.iso8601,
-       published_before: before.iso8601
-     }
-     service.list_searches(:snippet, opt)
+    !params[:keyword].blank?
+    require 'google/apis/youtube_v3'
+    youtube = Google::Apis::YoutubeV3::YouTubeService.new
+    youtube.key = ENV['YOUTUBE_1']
+    youtube_search_list = youtube.list_searches("id,snippet", type: "video",
+                                                            q: params[:keyword],
+                                                            max_results: 5)
+    search_result = youtube_search_list.to_h
+    @movies = search_result[:items]
+
     rescue Google::Apis::ClientError
       flash[:alert] = "１日の上限に達しました。"
       redirect_to(root_path)
     end
-    end
-  
-    def index
-      @youtube_data = find_videos('焼まゆるのお絵かきちゃんねる')
-      @post = Post.new
-    end
-  
-  
-    def indexx
-      begin
-      !params[:keyword].blank?
-      require 'google/apis/youtube_v3'
-      youtube = Google::Apis::YoutubeV3::YouTubeService.new
-      youtube.key = ENV['YOUTUBE_1']
-      youtube_search_list = youtube.list_searches("id,snippet", type: "video",
-                                                              q: params[:keyword],
-                                                              max_results: 5)
-      search_result = youtube_search_list.to_h
-      @movies = search_result[:items]
-  
-      rescue Google::Apis::ClientError
-        flash[:alert] = "１日の上限に達しました。"
-        redirect_to(root_path)
-      end
-  
-    end
 
   end
+
+end
