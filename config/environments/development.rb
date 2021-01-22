74a15ce6c4d2479d8d969bb0fa4fe3ecf0d9230a
gizaju10 <kaminomi3@gmail.com>
2021-01-23 06:30:47 +0900

    gem 'bullet'の利用

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,14 @@
 Rails.application.configure do
+  config.after_initialize do
+    Bullet.enable        = true #Bullet gemを有効
+    Bullet.alert         = true #ブラウザにJavaScriptアラートをポップアップ
+    Bullet.bullet_logger = true #Bulletログファイル（Rails.root/log/bullet.log）に記録
+    Bullet.console       = true #警告をブラウザーのconsole.logに記録
+  # Bullet.growl         = true
+    Bullet.rails_logger  = true #警告を直接Railsログに追加
+    Bullet.add_footer    = true #ページの左下に結果を表示
+  end
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
