38a3092f9c149514b220237f0c1908326874130c
gizaju10 <kaminomi3@gmail.com>
2021-01-31 15:52:05 +0900

    細部の修正等

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,17 @@
 //     $("p").text("jQuery稼働テスト(稼働中です)");
 //   });
 
+
+
+
+
+
+
+
+
+
 // 無限スクロール
-$(function() {
+$(document).on('turbolinks:load', function() {
   $('.jscroll').jscroll({
     contentSelector: '.skill-list',
     nextSelector: 'span.next:last a',
@@ -18,21 +27,10 @@ $(function() {
   });
 });
 
-// $(function() {
-//   $('.p-faq-title').on('click')({
-// 	$(this).toggleClass('active'),
-//   $(this).next().slideToggle(),
-//   });
-
-// $('.p-faq-title').on('click', function(){
-// 	$(this).toggleClass('active');
-// 	$(this).next().slideToggle();
-// });
-
 
 
 // 右下上へボタン
-$(function() {
+$(document).on('turbolinks:load', function() {
   var appear = false;
   var pagetop = $('#page_top');
   $(window).scroll(function () {
@@ -57,3 +55,63 @@ $(function() {
     return false;
   });
 });
+
+
+
+
+
+
+// 無限スクロール
+// $(function() {
+//   $('.jscroll').jscroll({
+//     contentSelector: '.skill-list',
+//     nextSelector: 'span.next:last a',
+//     loadingHtml: '<i class="fas fa-sync-alt fa-lg fa-spin"></i>'
+//   });
+// });
+
+
+// 右下上へボタン
+// $(function() {
+//   var appear = false;
+//   var pagetop = $('#page_top');
+//   $(window).scroll(function () {
+//     if ($(this).scrollTop() > 100) {  //100pxスクロールしたら
+//       if (appear == false) {
+//         appear = true;
+//         pagetop.stop().animate({
+//           'right': '5px' //右から5pxの位置に
+//         }, 300); //0.3秒かけて現れる
+//       }
+//     } else {
+//       if (appear) {
+//         appear = false;
+//         pagetop.stop().animate({
+//           'right': '-50px' //右から-50pxの位置に
+//         }, 300); //0.3秒かけて隠れる
+//       }
+//     }
+//   });
+//   pagetop.click(function () {
+//     $('body, html').animate({ scrollTop: 0 }, 500); //0.5秒かけてトップへ戻る
+//     return false;
+//   });
+// });
+
+
+
+
+
+
+
+// $(function() {
+//   $('.p-faq-title').on('click')({
+// 	$(this).toggleClass('active'),
+//   $(this).next().slideToggle(),
+//   });
+
+// $('.p-faq-title').on('click', function(){
+// 	$(this).toggleClass('active');
+// 	$(this).next().slideToggle();
+// });
+
