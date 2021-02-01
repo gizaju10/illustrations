1c6dd8d1fa8e774877f17e2fb80fbe0790bbd8d2
gizaju10 <kaminomi3@gmail.com>
2021-02-01 23:35:53 +0900

    gem 'factory_girl_rails'の削除

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
   # it "名前とメールアドレスとパスワードがあれば登録できる" do 
   #   expect(FactoryBot.create(:user)).to be_valid
   # end 
