0720387f69b87961f378f1b95029825a7ea4bbcb
gizaju10 <kaminomi3@gmail.com>
2021-03-13 06:04:44 +0900

    before_action :authenticate_user!を追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
 
   def create
     # @comment = Comment.new(comment_params)
@@ -19,17 +20,24 @@ class CommentsController < ApplicationController
   # コメント編集
   def edit
     @comment = Comment.find_by(id: params[:id])
+    # @post = Post.find_by(id: params[:id]) #この行いるのか？
   end
 
   # 投稿編集→投稿更新
   def update
+    # @post = Post.find_by(id: params[:id]) #この行いるのか？
     @comment = Comment.find_by(id: params[:id])
     @comment.content = params[:content]
     if @comment.save
-      flash[:notice] = "コメントを編集しました"
-      redirect_to("/posts")
+    # if @comment.update(comment_params)
+      flash[:notice] = "コメントを編集しました。"
+      # redirect_back(fallback_location: @post)
+      # redirect_to @comment
+      # redirect_to :back 
+      redirect_to("/posts/#{@comment.post_id}")
     else
-      render("/posts")
+      flash[:alert] = "コメントを編集できませんでした。"
+      redirect_back(fallback_location: @post)
     end
   end
 
@@ -47,6 +55,7 @@ class CommentsController < ApplicationController
   private
     def comment_params
       # params.require(:comment).permit(:content, :post_id)
-      params.require(:comment).permit(:content, :item_id, :user_id) #追加
+      # params.require(:comment).permit(:content, :item_id, :user_id) #追加
+      params.require(:comment).permit(:content, :post_id, :user_id) #追加
     end
   end
