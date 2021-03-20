a51403011b745b5857f5d18d8f0a89fcc5489528
gizaju10 <kaminomi3@gmail.com>
2021-03-21 05:02:27 +0900

    Rspecで、FactoryBotを有効利用

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,18 @@
-FactoryBot.define do 
-  factory :user do #factory :testuser, class: User do のようにクラスを明示すればモデル名以外のデータも作れます。
-    name { "test" }
-    sequence(:email) { |n| "TEST#{n}@example.com" }
-    password { "testuser" }
+# FactoryBot.define do 
+#   factory :user do #factory :testuser, class: User do のようにクラスを明示すればモデル名以外のデータも作れます。
+#     name { "test" }
+#     sequence(:email) { |n| "TEST#{n}@example.com" }
+#     password { "testuser" }
+#   end
+# end
+
+FactoryBot.define do
+
+  factory :user do
+    name                  {"テストユーザー"}
+    email                 {"abcde@gmail.com"}
+    password              {"aaaaaa"}
+    password_confirmation {"aaaaaa"}
+    profile               {"初めまして。"}
   end
-end 
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,34 +1,69 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
-  # it "名前とメールアドレスとパスワードがあれば登録できる" do 
-  #   expect(FactoryBot.create(:user)).to be_valid
-  # end 
+# RSpec.describe User, type: :model do
+#   # pending "add some examples to (or delete) #{__FILE__}"
+#   # it "名前とメールアドレスとパスワードがあれば登録できる" do 
+#   #   expect(FactoryBot.create(:user)).to be_valid
+#   # end 
 
-  it "名前がなければ登録できない" do 
-    expect(FactoryBot.build(:user, name: "")).to_not be_valid 
-  end 
+#   it "名前がなければ登録できない" do 
+#     expect(FactoryBot.build(:user, name: "")).to_not be_valid 
+#   end 
 
-  it "メールアドレスがなければ登録できない" do 
-    expect(FactoryBot.build(:user, email: "")).to_not be_valid 
-  end 
+#   it "メールアドレスがなければ登録できない" do 
+#     expect(FactoryBot.build(:user, email: "")).to_not be_valid 
+#   end 
 
-  # it "メールアドレスが重複していたら登録できない" do 
-  #   user1 = FactoryBot.create(:user,name: "taro", email: "taro@example.com")
-  #   expect(FactoryBot.build(:user, name: "ziro", email: user1.email)).to_not be_valid
-  # end 
+#   # it "メールアドレスが重複していたら登録できない" do 
+#   #   user1 = FactoryBot.create(:user,name: "taro", email: "taro@example.com")
+#   #   expect(FactoryBot.build(:user, name: "ziro", email: user1.email)).to_not be_valid
+#   # end 
 
-  it "パスワードがなければ登録できない" do 
-    expect(FactoryBot.build(:user, password: "")).to_not be_valid 
-  end 
+#   it "パスワードがなければ登録できない" do 
+#     expect(FactoryBot.build(:user, password: "")).to_not be_valid 
+#   end 
 
-  # it "パスワードが暗号化されているか" do 
-  #   user = FactoryBot.create(:user)
-  #   expect(user.password_digest).to_not eq "password"
-  # end 
+#   # it "パスワードが暗号化されているか" do 
+#   #   user = FactoryBot.create(:user)
+#   #   expect(user.password_digest).to_not eq "password"
+#   # end 
 
-  it "password_confirmationとpasswordが異なる場合保存できない" do 
-    expect(FactoryBot.build(:user,password:"password",password_confirmation: "passward")).to_not be_valid 
-  end 
+#   it "password_confirmationとpasswordが異なる場合保存できない" do 
+#     expect(FactoryBot.build(:user,password:"password",password_confirmation: "passward")).to_not be_valid 
+#   end 
+
+  describe User do
+    describe '#create' do
+      # 入力されている場合のテスト ▼
+  
+      it "全ての項目の入力が存在すれば登録できること" do # テストしたいことの内容
+        user = build(:user)  # 変数userにbuildメソッドを使用して、factory_botのダミーデータを代入
+        expect(user).to be_valid # 変数userの情報で登録がされるかどうかのテストを実行
+      end
+    end
+
+    it "nameがない場合は登録できないこと" do # テストしたいことの内容
+      user = build(:user, name: nil) # 変数userにbuildメソッドを使用して、factory_botのダミーデータを代入(今回の場合は意図的にnicknameの値をからに設定)
+      user.valid? #バリデーションメソッドを使用して「バリデーションにより保存ができない状態であるか」をテスト
+      expect(user.errors[:name]).to include("が入力されていません。") # errorsメソッドを使用して、「バリデーションにより保存ができない状態である場合なぜできないのか」を確認し、.to include("が入力されていません。")でエラー文を記述(今回のRailsを日本語化しているのでエラー文も日本語)
+    end
+
+    it "emailがない場合は登録できないこと" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("が入力されていません。")
+    end
+
+    it "passwordがない場合は登録できないこと" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("が入力されていません。")
+    end
+
+    it "パスワードと確認が一致していないと登録できない" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません。")
+    end
+
 end
