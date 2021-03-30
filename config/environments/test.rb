df276d5299af3cb37a771f6077e611f302e1ac94
gizaju10 <kaminomi3@gmail.com>
2021-03-30 10:14:59 +0900

    circleCIでRSpecが通らない為、一部変更

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,7 +5,7 @@
 
 Rails.application.configure do
   config.after_initialize do
-    Bullet.enable        = true
+    Bullet.enable        = false
     Bullet.bullet_logger = true
     Bullet.raise         = true # raise an error if n+1 query occurs
   end
