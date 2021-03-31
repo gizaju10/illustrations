5542a1ee4fc2d395d8a4f56d144d346a8d576170
gizaju10 <kaminomi3@gmail.com>
2021-03-31 21:22:23 +0900

    valiadatesのメッセージ変更

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -3,7 +3,7 @@ class Inquiry < ApplicationRecord
 
     attr_accessor :name, :email, :message
   
-    validates :name, presence: {message: 'を入力してください'}
-    validates :email, presence: {message: 'を入力してください'}
-    validates :message, presence: {message: 'を入力してください'}
+    validates :name, presence: {message: 'を入力してください。'}
+    validates :email, presence: {message: 'を入力してください。'}
+    validates :message, presence: {message: 'を入力してください。'}
 end
