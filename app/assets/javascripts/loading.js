f110a534a39e7a7cf7a550f7ca1d06a038296c16
gizaju10 <kaminomi3@gmail.com>
2021-04-12 08:24:53 +0900

    ソースコードの整形済

--- app/assets/javascripts/loading.js
+++ app/assets/javascripts/loading.js
@@ -1,15 +1,15 @@
-$(document).on('turbolinks:load', function() {
-    $(function(){
-      var loader = $('.loader-wrap');
-  
-      //ページの読み込みが完了したらアニメーションを非表示
-      $(window).on('load',function(){
-        loader.fadeOut();
-      });
-  
-      //ページの読み込みが完了してなくても5秒後にアニメーションを非表示にする
-      setTimeout(function(){
-        loader.fadeOut();
-      },5000);
+$(document).on('turbolinks:load', function () {
+  $(function () {
+    var loader = $('.loader-wrap');
+
+    //ページの読み込みが完了したらアニメーションを非表示
+    $(window).on('load', function () {
+      loader.fadeOut();
     });
+
+    //ページの読み込みが完了してなくても5秒後にアニメーションを非表示にする
+    setTimeout(function () {
+      loader.fadeOut();
+    }, 5000);
   });
+});
