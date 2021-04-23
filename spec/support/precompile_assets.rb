5c6ca6fd041077ff8999f5064ae8f6139dab9eeb
gizaju10 <kaminomi3@gmail.com>
2021-04-23 13:44:21 +0900

    試し

--- spec/support/precompile_assets.rb
+++ spec/support/precompile_assets.rb
@@ -26,8 +26,8 @@ RSpec.configure do |config|
           # next 3 lines to compile webpacker before running our test suite
           require "rake"
           Rails.application.load_tasks
-          Rake::Task["assets:clobber"].execute
-          Rake::Task["assets:precompile"].execute
+          # Rake::Task["assets:clobber"].execute
+          # Rake::Task["assets:precompile"].execute
         ensure
           $stdout.reopen(original_stdout)
           $stdout.puts "Finished in #{(Time.current - start).round(2)} seconds"
