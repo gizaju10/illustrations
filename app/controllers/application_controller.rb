a854d4f9e67f7959bda6802007f24f756c8b1854
gizaju10 <kaminomi3@gmail.com>
2021-04-24 19:27:02 +0900

    不要なコメントアウトの削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :tags
   protect_from_forgery with: :exception
 
-  # ログイン済ユーザーのみにアクセスを許可する
-  # before_action :authenticate_user!
-
   # deviseコントローラーにストロングパラメータを追加する          
   before_action :configure_permitted_parameters, if: :devise_controller?
 
