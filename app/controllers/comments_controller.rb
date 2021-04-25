22888c65d4b80fc52ec5555543105ab84f272b4d
gizaju10 <kaminomi3@gmail.com>
2021-04-25 14:52:00 +0900

    不要なコメントアウトの削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,6 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    # @comment = Comment.new(comment_params)
-    # @comment.user_id = current_user.id
-    # if @comment.save
-    #   redirect_back(fallback_location: root_path)
-    # else
-    #   redirect_back(fallback_location: root_path)
-    # end
     @post = Post.find(params[:post_id])
     #投稿に紐づいたコメントを作成
     @comment = @post.comments.build(comment_params)
@@ -20,20 +13,14 @@ class CommentsController < ApplicationController
   # コメント編集
   def edit
     @comment = Comment.find_by(id: params[:id])
-    # @post = Post.find_by(id: params[:id]) #この行いるのか？
   end
 
   # 投稿編集→投稿更新
   def update
-    # @post = Post.find_by(id: params[:id]) #この行いるのか？
     @comment = Comment.find_by(id: params[:id])
     @comment.content = params[:content]
     if @comment.save
-    # if @comment.update(comment_params)
       flash[:notice] = "コメントを編集しました。"
-      # redirect_back(fallback_location: @post)
-      # redirect_to @comment
-      # redirect_to :back 
       redirect_to("/posts/#{@comment.post_id}")
     else
       flash[:alert] = "コメントを編集できませんでした。"
@@ -43,10 +30,6 @@ class CommentsController < ApplicationController
 
   # コメント編集→コメント削除
   def destroy
-    # @comment = Comment.find_by(id: params[:id])
-    # @comment.destroy
-    # flash[:notice] = "コメントを削除しました"
-    # redirect_to("/")
     @comment = Comment.find(params[:id])
     @comment.destroy
     render :index
@@ -54,8 +37,6 @@ class CommentsController < ApplicationController
   
   private
     def comment_params
-      # params.require(:comment).permit(:content, :post_id)
-      # params.require(:comment).permit(:content, :item_id, :user_id) #追加
       params.require(:comment).permit(:content, :post_id, :user_id) #追加
     end
   end
