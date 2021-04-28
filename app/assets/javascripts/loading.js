83813925307ce82c04ae81855e79b05bb5e9cbf9
gizaju10 <kaminomi3@gmail.com>
2021-04-28 15:39:10 +0900

    ローディングの表示を最大3秒までに変更

--- app/assets/javascripts/loading.js
+++ app/assets/javascripts/loading.js
@@ -7,9 +7,9 @@ $(document).on('turbolinks:load', function () {
       loader.fadeOut();
     });
 
-    //ページの読み込みが完了してなくても5秒後にアニメーションを非表示にする
+    //ページの読み込みが完了してなくても3秒後にアニメーションを非表示にする
     setTimeout(function () {
       loader.fadeOut();
-    }, 5000);
+    }, 3000);
   });
 });
