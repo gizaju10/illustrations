92a43a2f8e8586d2e831f04f7bb8501710fac2af
gizaju10 <kaminomi3@gmail.com>
2021-04-29 00:27:19 +0900

    開発環境の画像保存をS3→ローカルに変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,5 @@
 unless Rails.env.development? || Rails.env.test?
-# unless Rails.env.test?
   CarrierWave.configure do |config|
-  # if Rails.env.production? # 本番環境:AWS使用
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
@@ -11,8 +9,5 @@ unless Rails.env.development? || Rails.env.test?
 
     config.fog_directory  = ENV['AWS_S3']
     config.cache_storage = :fog
-  # else
-  #   config.storage :file # 開発環境:public/uploades下に保存
-  #   config.enable_processing = false if Rails.env.test? #test:処理をスキップ
   end
 end
