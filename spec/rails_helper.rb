5f0e6e11226a5aae2836e9ebb4603b8d43aefd01
gizaju10 <kaminomi3@gmail.com>
2021-05-09 08:50:54 +0900

    不必要なコメントアウトの削除

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -53,68 +53,6 @@ RSpec.configure do |config|
   #       # ...
   #     end
 
-
-
-
-  # config.before(:each, type: :system) do
-  #   driven_by :rack_test
-  # end
-
-  # config.before(:each, type: :system, js: true) do
-  #   driven_by :headless_chrome, screen_size: [1920, 1080]
-  # end
-
-
-
-
-
-  # Capybara.register_driver :remote_chrome do |app|
-  #   url = "http://chrome:4444/wd/hub"
-  #   caps = ::Selenium::WebDriver::Remote::Capabilities.chrome(
-  #     "goog:chromeOptions" => {
-  #       "args" => [
-  #         "no-sandbox",
-  #         "headless",
-  #         "disable-gpu",
-  #         "window-size=1680,1050"
-  #       ]
-  #     }
-  #   )
-  #   Capybara::Selenium::Driver.new(app, browser: :remote, url: url, desired_capabilities: caps)
-  # end
-  
-  # RSpec.configure do |config|
-  
-  #   config.before(:each, type: :system) do
-  #     driven_by :rack_test
-  #   end
-  
-  #   config.before(:each, type: :system, js: true) do
-  #     driven_by :remote_chrome
-  #     Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
-  #     Capybara.server_port = 3000
-  #     Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
-  #   end
-  # # ~~~
-  # end
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  #
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
