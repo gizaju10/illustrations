552667e8338748e2030fc432b0fee6fa874a660b
gizaju10 <kaminomi3@gmail.com>
2021-05-10 16:58:47 +0900

    お問い合わせの:messageの文字制限を1000文字に変更

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -5,5 +5,5 @@ class Inquiry < ApplicationRecord
   
     validates :name, presence: {message: 'を入力してください。'}
     validates :email, presence: {message: 'を入力してください。'}
-    validates :message, presence: {message: 'を入力してください。'}
+    validates :message, presence: {message: 'を入力してください。'}, length: {maximum: 1000}
 end
