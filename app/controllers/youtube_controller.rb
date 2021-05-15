4be283fe02a85e42cc047f19a6e44a98c960bca9
gizaju10 <kaminomi3@gmail.com>
2021-05-16 01:41:13 +0900

    max_results: 8 → 10 に変更等

--- app/controllers/youtube_controller.rb
+++ app/controllers/youtube_controller.rb
@@ -8,55 +8,55 @@ def search
   # flash[:alert] = "1"〜"10"は開発環境時に適宜使用         
   begin
     youtube.key = ENV['YOUTUBE_1']
-    youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: params[:keyword], max_results: 8)
+    youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: params[:keyword], max_results: 10)
     # flash[:alert] = "1"
   rescue Google::Apis::ClientError
     begin
       youtube.key = ENV['YOUTUBE_2']
-      youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: params[:keyword], max_results: 8)
+      youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: params[:keyword], max_results: 10)
       # flash[:alert] = "2"
     rescue Google::Apis::ClientError
       begin
         youtube.key = ENV['YOUTUBE_3']
-        youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: params[:keyword], max_results: 8)
+        youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: params[:keyword], max_results: 10)
         # flash[:alert] = "3"
       rescue Google::Apis::ClientError
           begin
             youtube.key = ENV['YOUTUBE_4']
-            youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: params[:keyword], max_results: 8)
+            youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: params[:keyword], max_results: 10)
             # flash[:alert] = "4"
           rescue Google::Apis::ClientError
             begin
               youtube.key = ENV['YOUTUBE_5']
-              youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: params[:keyword], max_results: 8)
+              youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: params[:keyword], max_results: 10)
               # flash[:alert] = "5"
             rescue Google::Apis::ClientError
               begin
                 youtube.key = ENV['YOUTUBE_6']
-                youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: params[:keyword], max_results: 8)
+                youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: params[:keyword], max_results: 10)
                 # flash[:alert] = "6"
               rescue Google::Apis::ClientError
                 begin
                   youtube.key = ENV['YOUTUBE_7']
-                  youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: params[:keyword], max_results: 8)
+                  youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: params[:keyword], max_results: 10)
                   # flash[:alert] = "7"
                 rescue Google::Apis::ClientError
                   begin
                     youtube.key = ENV['YOUTUBE_8']
-                    youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: params[:keyword], max_results: 8)
+                    youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: params[:keyword], max_results: 10)
                     # flash[:alert] = "8"
                   rescue Google::Apis::ClientError
                     begin
                       youtube.key = ENV['YOUTUBE_9']
-                      youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: params[:keyword], max_results: 8)
+                      youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: params[:keyword], max_results: 10)
                       # flash[:alert] = "9"
                     rescue Google::Apis::ClientError
                       begin
                         youtube.key = ENV['YOUTUBE_10']
-                        youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: params[:keyword], max_results: 8)
+                        youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: params[:keyword], max_results: 10)
                         # flash[:alert] = "10"
                       rescue Google::Apis::ClientError
-                        flash[:alert] = "１日の上限に達しました。新規投稿の際は、https://www.youtube.com/から動画を検索してご利用下さい。"
+                        flash[:alert] = "１日の上限に達しました。新規投稿の際は、https://www.youtube.com から動画を検索してご利用下さい。"
                         redirect_to(posts_path)
                       end
                     end
