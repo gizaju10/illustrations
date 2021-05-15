baefd21c00e195dc49d27ea69e5ef16afb97dd33
gizaju10 <kaminomi3@gmail.com>
2021-05-16 01:34:08 +0900

    不要なコメントアウトの削除

--- app/models/post.rb
+++ app/models/post.rb
@@ -45,14 +45,11 @@ class Post < ApplicationRecord
   # コメント通知
   def create_notification_comment!(current_user, comment_id)
     # 自分以外にコメントしている人をすべて取得し、全員に通知を送る
-    # temp_ids = Comment.select(:user_id).where(post_id: id).where.not(user_id: current_user.id).distinct
     temp_ids = Comment.select(:user_id).where(post_id: id).where.not(user_id: current_user.id).distinct
     save_notification_comment!(current_user, comment_id, user_id) # 投稿者への通知
     temp_ids.each do |temp_id|
       save_notification_comment!(current_user, comment_id, temp_id['user_id'])
     end
-    # まだ誰もコメントしていない場合は、投稿者に通知を送る
-    # save_notification_comment!(current_user, comment_id, user_id) if temp_ids.blank?
   end
 
   def save_notification_comment!(current_user, comment_id, visited_id)
